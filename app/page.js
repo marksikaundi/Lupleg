7521605b88fb26cf0b389a81f8bf8204a9ba861f
marksikaundi <emmanuelmark117@gmail.com>
2024-02-20 21:19:59 +0200

    smal

--- app/page.js
+++ app/page.js
@@ -10,16 +10,16 @@ export default function Component() {
         <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-gray-800">
           <span className="block xl:inline">The efficiency of help and</span>{" "}
           <span className="block text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-orange-400 xl:inline">
-            Collaboration with 
+            Collaboration with
           </span>{" "}
           <span className="block xl:inline">Developers</span>{" "}
           <span className="block text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 xl:inline">
-          empowers
+            empowers
           </span>
         </h1>
         <p className="mt-4 pt-12 pb-8 text-base sm:text-lg md:text-2xl text-gray-600">
-          Lupleg is a community platform that enables teams to
-          streamline and help novice scale up their career in tech.
+          Lupleg is a community platform that enables teams to streamline and
+          help novice scale up their career in tech.
         </p>
         <Button className="W-15 mt-8 px-8 py-3 text-md bg-[#ED7133] text-white rounded-full p-8 hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange focus:ring-opacity-50 transition duration-300">
           <Link href="contacts">Get started</Link>
