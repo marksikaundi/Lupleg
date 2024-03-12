0a038ae823e76aa9a1ba0a505bf086c3d67be752
marksikaundi <emmanuelmark117@gmail.com>
2024-03-12 17:59:03 +0200

    improving the cards

--- components/FeaturesCards/Cards.jsx
+++ components/FeaturesCards/Cards.jsx
@@ -1,16 +1,21 @@
-export default function Card() {
+export default function Cards() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
           <div>
             <h1 className="text-5xl font-extrabold tracking-tight text-gray-900 sm:text-6xl">
-              It's not or. It's and.
+              Community Driven Content
             </h1>
             <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Choosing a customer communication solution is hard — but with
-              Front, it's not. Front is the only inbox built for collaboration,
-              visibility, and efficiency at scale.
+              <b>In-depth Tutorials:</b> Create detailed guides on specific
+              technologies, frameworks, or coding challenges relevant to your
+              development projects.
+            </p>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+              <b>Code Examples and Solutions:</b> Share code snippets or open-source
+              projects that showcase best practices and solutions to common
+              problems.
             </p>
           </div>
           <div className="bg-gradient-to-br from-[#fde68a] to-[#fca5a5] p-8 rounded-xl">
