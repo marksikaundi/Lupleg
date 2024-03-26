ed7a708a5653f3d046741d96e2848978d5efd719
Vikram <vikram.gupta.2513@gmail.com>
2024-03-26 15:19:56 +0530

    Newsletter section styling issue fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -60,16 +60,15 @@ export default function Footer() {
         {/* Newsletter  */}
         <div>
           <h5 className="mb-4 font-bold">Newsletter</h5>
-          <div className="flex flex-col space-y-4">
-            <div className="flex items-center">
-              <Input
-                className="rounded-md"
-                placeholder="Enter Your Email Here"
-              />
-              <Button className="ml-2 bg-orange-700 hover:bg-orange-950">
-                Subscribe
-              </Button>
-            </div>
+          <div className="flex flex-col items-center space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+            <Input
+              className="rounded-md text-black"
+              placeholder="Enter Your Email Here"
+              type="text"
+            />
+            <Button className="w-full md:w-auto bg-orange-700 hover:bg-orange-900">
+              Subscribe
+            </Button>
           </div>
           <div className="mt-8">
             <h5 className="mb-4 font-bold">Helpdesk</h5>
@@ -128,8 +127,7 @@ function FacebookIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round"
-    >
+      strokeLinejoin="round">
       <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
     </svg>
   );
@@ -147,8 +145,7 @@ function LinkedinIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round"
-    >
+      strokeLinejoin="round">
       <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
       <rect width="4" height="12" x="2" y="9" />
       <circle cx="4" cy="4" r="2" />
@@ -168,8 +165,7 @@ function TwitterIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round"
-    >
+      strokeLinejoin="round">
       <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
     </svg>
   );
