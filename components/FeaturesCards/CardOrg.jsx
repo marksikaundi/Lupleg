6f6d44380ece5719ceaecfdb24e82acb171b6cf1
Vikram <vikram.gupta.2513@gmail.com>
2024-03-27 19:58:56 +0530

    org tab components added

---
+++ components/FeaturesCards/CardOrg.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const CardOrg = ({ title, subtitle, description, imageUrl, learnMoreLink }) => {
+  return (
+    <div className="w-full bg-transparent">
+      <div className="flex flex-col md:flex-row lg:gap-20 gap-8 md:gap-12 container mx-auto lg:py-28 py-24 px-8">
+        <div className="container flex flex-col md:w-1/2 2xl:gap-5 xl:gap-4 lg:gap-3 md:gap-2 gap-3">
+          <div>
+            <p className="lg:mb-2 xl:mb-3 2xl:mb-4 md:text-lg">{title}</p>
+            <h2 className="font-semibold sm:text-2xl text-xl md:text-xl lg:text-3xl xl:text-4xl 2xl:text-6xl">
+              {subtitle}
+            </h2>
+          </div>
+          <div>
+            <p className="lg:text-sm xl:text-lg 2xl:text-xl md:text-xs">
+              {description}
+            </p>
+          </div>
+          <div>
+            <a href={learnMoreLink}>Learn more →</a>
+          </div>
+        </div>
+        <div className="md:w-1/2">
+          <img src={imageUrl} className="object-cover" alt="Talent Image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardOrg;
