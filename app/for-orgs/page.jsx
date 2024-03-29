3315b5c636207df7db6e59758698af4b3f07ec6e
marksikaundi <emmanuelmark117@gmail.com>
2024-03-29 16:23:12 +0200

    updated the cards with prescriptive namings

--- app/for-orgs/page.jsx
+++ app/for-orgs/page.jsx
@@ -1,6 +1,6 @@
-import CardOrg from "@/components/FeaturesCards/CardOrg";
-import CardOrg2 from "@/components/FeaturesCards/CardOrg2";
 import React from "react";
+import CardSection from "@/components/FeaturesCards/CardSection";
+import CardSections from "@/components/FeaturesCards/CardSections";
 
 function Organization() {
   return (
@@ -12,15 +12,17 @@ function Organization() {
               We’re In The Business of Changing Lives
             </p>
             <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-gray-800 mb-10">
-          <span className="block xl:inline">The efficiency of help and</span>{" "}
-          <span className="block text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-orange-400 xl:inline">
-            Collaboration with
-          </span>{" "}
-          <span className="block xl:inline">Developers</span>{" "}
-          <span className="block text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 xl:inline">
-            empowers
-          </span>
-        </h1>
+              <span className="block xl:inline">
+                The efficiency of help and
+              </span>{" "}
+              <span className="block text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-orange-400 xl:inline">
+                Collaboration with
+              </span>{" "}
+              <span className="block xl:inline">Developers</span>{" "}
+              <span className="block text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 xl:inline">
+                empowers
+              </span>
+            </h1>
           </div>
           <div className="flex justify-center">
             <img
@@ -31,21 +33,21 @@ function Organization() {
         </div>
       </div>
 
-      <CardOrg
+      <CardSections
         title="Hire Talents from Zuri"
         subtitle="Unlock the Potential of Zuri Graduates for Your Organization"
         description="At Zuri, we are committed to nurturing exceptional talent in the tech industry. Our comprehensive training programs equip individuals with cutting-edge skills and empower them to excel in their careers."
         imageUrl="https://assets-global.website-files.com/63e4cc8d2ac61a3dee9bb0ee/63e5184308199cf4867fa2ba_Group%20151-p-800.webp"
         learnMoreLink="a"
       />
-      <CardOrg2
+      <CardSection
         title="Hire Talents from Zuri"
         subtitle="Unlock the Potential of Zuri Graduates for Your Organization"
         description="At Zuri, we are committed to nurturing exceptional talent in the tech industry. Our comprehensive training programs equip individuals with cutting-edge skills and empower them to excel in their careers."
         imageUrl="https://assets-global.website-files.com/63e4cc8d2ac61a3dee9bb0ee/63e5184308199cf4867fa2ba_Group%20151-p-800.webp"
         learnMoreLink="a"
       />
-      <CardOrg
+      <CardSections
         title="Hire Talents from Lupleg"
         subtitle="Unlock the Potential of Zuri Graduates for Your Organization"
         description="At Zuri, we are committed to nurturing exceptional talent in the tech industry. Our comprehensive training programs equip individuals with cutting-edge skills and empower them to excel in their careers."
