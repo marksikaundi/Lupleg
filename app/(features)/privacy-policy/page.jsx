4a8f15b2e5d7f2d363a3215a41d190f86d545f84
marksikaundi <emmanuelmark117@gmail.com>
2024-03-30 15:15:36 +0200

    updating legal pages

---
+++ app/(features)/privacy-policy/page.jsx
@@ -0,0 +1,49 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="bg-white flex flex-col items-center container px-4 mx-auto">
+      <section className="py-12 md:py-16 lg:py-20 xl:py-24">
+        <h1 class="text-black text-center text-4xl font-bold tracking-tight md:text-5xl mb-12">
+          Privacy Policy
+        </h1>
+        <p class="max-w-2xl items-center justify-center flex-1">
+          Your privacy is important to us. It is Lupleg &#x27;s policy to
+          respect your privacy regarding any information we may collect from you
+          across our website, https://lupleg.website, and other sites we own and
+          operate. <br />
+          <br />
+          We only ask for personal information when we truly need it to provide
+          a service to you. We collect it by fair and lawful means, with your
+          knowledge and consent. We also let you know why we &#x27;re collecting
+          it and how it will be used. <br />
+          <br />
+          We only retain collected information for as long as necessary to
+          provide you with your requested service. What data we store, we
+          &#x27;ll protect within commercially acceptable means to prevent loss
+          and theft, as well as unauthorised access, disclosure, copying, use or
+          modification. <br />
+          <br />
+          We don &#x27;t share any personally identifying information publicly
+          or with third-parties, except when required to by law.
+          <br />
+          <br />
+          Our website may link to external sites that are not operated by us.
+          Please be aware that we have no control over the content and practices
+          of these sites, and cannot accept responsibility or liability for
+          their respective privacy policies. <br />
+          <br />
+          You are free to refuse our request for your personal information (such
+          as your email), with the understanding that we may be unable to
+          provide you with some of your desired services. <br />
+          <br />
+          Your continued use of our website will be regarded as acceptance of
+          our practices around privacy and personal information. If you have any
+          questions about how we handle user data and personal information, feel
+          free to contact us. <br />
+          <br />
+          This policy is effective as of 29 January 2024. <br />
+          <br />
+        </p>
+      </section>
+    </div>
+  );
+}
