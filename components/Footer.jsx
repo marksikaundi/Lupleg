a8b3fff4ea410b8d687cb70125d28aaf0b49eaf6
marksikaundi <emmanuelmark117@gmail.com>
2024-03-30 15:48:01 +0200

    updated the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,25 +1,25 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-// bg-[#370C0C]
+
 export default function Footer() {
   return (
     <footer className="bg-green-950 p-10 text-white">
       <div className="mx-auto grid max-w-7xl grid-cols-2 gap-10 md:grid-cols-4">
         <div>
-          <h5 className="mb-4 font-bold">Lupleg LLC</h5>
+          <h5 className="mb-4 font-bold">Company</h5>
           <ul className="space-y-3">
             <li>
               <Link href="business">For Businesses</Link>
             </li>
             <li>
-              <Link href="for-orgs">For NGOs</Link>
+              <Link href="for-orgs">For ORGs</Link>
             </li>
             <li>
-              <Link href="trainings">Trainings</Link>
+              <Link href="challenge">Challenge</Link>
             </li>
             <li>
-              <Link href="affiliatesclear">Affiliates</Link>
+              <Link href="https://mms.lupleg.website">MMS</Link>
             </li>
           </ul>
         </div>
@@ -50,10 +50,10 @@ export default function Footer() {
               <Link href="our-why">Our Why</Link>
             </li>
             <li>
-              <Link href="faq">FAQ</Link>
+              <Link href="https://docs.lupleg.website">Docs</Link>
             </li>
             <li>
-              <Link href="https://youtube.com/lupleg">Youtube Playlist</Link>
+              <Link href="faq">FAQ</Link>
             </li>
           </ul>
         </div>
@@ -127,7 +127,8 @@ function FacebookIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round">
+      strokeLinejoin="round"
+    >
       <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
     </svg>
   );
@@ -145,7 +146,8 @@ function LinkedinIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round">
+      strokeLinejoin="round"
+    >
       <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
       <rect width="4" height="12" x="2" y="9" />
       <circle cx="4" cy="4" r="2" />
@@ -165,7 +167,8 @@ function TwitterIcon(props) {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round">
+      strokeLinejoin="round"
+    >
       <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
     </svg>
   );
