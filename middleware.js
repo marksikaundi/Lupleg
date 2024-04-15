2ce07b8d5d44948d81b9cfc64619e7c38c5abda8
marksikaundi <emmanuelmark117@gmail.com>
2024-04-15 13:13:30 +0200

    Remove sign-in and sign-up pages using Clerk

--- middleware.js
+++ middleware.js
@@ -1,7 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default authMiddleware({});
+const protectedRoute = createRouteMatcher([
+  '/dashboard'
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
