9b9886a7af111f71882f5b5d277890948df63806
Vikram <vikram.gupta.2513@gmail.com>
2024-04-15 18:13:45 +0530

    solve public route issue

--- middleware.js
+++ middleware.js
@@ -1,6 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/challenge",
+    "/services",
+    "/academy",
+    "/podcasts",
+    "/for-orgs",
+  ],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
