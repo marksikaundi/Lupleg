b38ddefaf2a8655203a2e23934bc5776927865f0
marksikaundi <emmanuelmark117@gmail.com>
2024-06-17 05:46:04 +0200

    chore: Remove unused authentication and premium pages

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,14 +21,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
+        <ClerkProvider>
         <body className={inter.className}>
           <Header />
           {children}
           <Footer />
         </body>
+        </ClerkProvider >
       </html>
   );
 }
 
 
-// For reference use this link: https://icodethis.com/pro
