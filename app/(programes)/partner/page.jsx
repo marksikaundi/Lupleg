02bef631c3cb9dfd3b619e96695b1fc17f57f0f9
marksikaundi <emmanuelmark117@gmail.com>
2024-06-25 00:07:01 +0200

    removed contents

--- app/(programes)/partner/page.jsx
+++ app/(programes)/partner/page.jsx
@@ -11,10 +11,10 @@ export default function Partnership() {
           <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
             <div>
               <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                Partner with Lupleg EduSaaS
+                Partner with Lupleg 
               </h1>
               <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                Lupleg is a leading provider of educational SaaS solutions. Join
+                Lupleg is a leading provider of educational solutions. Join
                 our partner network and help schools and districts transform
                 their learning experiences.
               </p>
