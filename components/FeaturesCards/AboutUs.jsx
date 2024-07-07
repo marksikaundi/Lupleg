22572d4e806bc08432d4c98f482f64849bd8dcf2
marksikaundi <emmanuelmark117@gmail.com>
2024-07-07 15:33:02 +0200

    UPDATED: fonts, padding and margin fixed with bg-green-400

--- components/FeaturesCards/AboutUs.jsx
+++ components/FeaturesCards/AboutUs.jsx
@@ -7,7 +7,7 @@ export default function MoreAboutUs() {
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
-              <div className="inline-block rounded-lg bg-[#122E17] px-3 py-1 text-sm text-white">
+              <div className="inline-block rounded-lg bg-green-400 px-4 py-4 text-md font-mono text-green-900">
                 Our Services
               </div>
               <h2 className="text-3xl font-bold tracking-tighter text-gray-900 dark:text-white sm:text-5xl">
@@ -104,7 +104,7 @@ export default function MoreAboutUs() {
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
-              <div className="inline-block rounded-lg bg-[#122E17] px-3 py-1 text-sm text-white">
+              <div className="inline-block rounded-lg bg-green-400 px-4 py-4 text-md font-mono text-green-900">
                 Why Choose Us?
               </div>
               <h2 className="text-3xl font-bold tracking-tighter text-gray-900 dark:text-white sm:text-5xl">
