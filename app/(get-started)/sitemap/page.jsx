fda50a01b9060323bfaeee14abb6dd842edd393d
marksikaundi <emmanuelmark117@gmail.com>
2024-07-09 14:37:52 +0200

    UPDATED: styles

--- app/(get-started)/sitemap/page.jsx
+++ app/(get-started)/sitemap/page.jsx
@@ -91,15 +91,15 @@ export default function Sitemap() {
             </div>
             <div className="flex justify-center flex-col sm:flex-row items-start gap-4">
               <Link
-                href="#"
-                className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                href="contact"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-[#C24229] px-8 text-sm font-medium text-gray-50 shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                 prefetch={false}
               >
                 Contact Sales
               </Link>
               <Link
-                href="#"
-                className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                href="https://app.lupleg.website/dashboard"
+                className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-green-900 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
                 prefetch={false}
               >
                 Tour the Platform
@@ -114,8 +114,8 @@ export default function Sitemap() {
                 Experience the workflow the best frontend teams love.
               </h2>
               <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                Let your team focus on shipping features instead of managing
-                infrastructure with automated CI/CD.
+                Let your team focus on bussiness prospects instead of managing
+                development and managing infrastructures
               </p>
             </div>
             <div className="mx-auto w-full max-w-sm space-y-2">
@@ -125,13 +125,13 @@ export default function Sitemap() {
                   placeholder="Enter your email"
                   className="max-w-lg flex-1"
                 />
-                <Button type="submit">Sign Up</Button>
+                <Button type="submit" className="bg-[#C24229] hover:bg-[#C24229] ">Sign Up</Button>
               </form>
               <p className="text-xs text-gray-500 dark:text-gray-400">
                 Sign up to get notified when we launch.
                 <Link
-                  href="#"
-                  className="underline underline-offset-2"
+                  href="/terms"
+                  className="underline underline-offset-2 text-green-700"
                   prefetch={false}
                 >
                   Terms &amp; Conditions
