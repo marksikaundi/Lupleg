d756773fb1474bf65df02314d3717832f6a89a4e
marksikaundi <emmanuelmark117@gmail.com>
2024-07-19 17:06:13 +0200

    refactor: Add lazy loading for image in Hero component

--- app/(Development)/new/page.jsx
+++ app/(Development)/new/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
-export default function New() {
+export default function Hero() {
   return (
-    <div>
-      <h2>New Landing page</h2>
+    <div className="relative w-full h-screen">
+      
+
+        {/* Bottom Right Image */}
+        <Image
+        src="https://utfs.io/f/ff7c57f5-a1b0-4c0c-afde-390ecdf9c87e-6uzm02.jpg"
+        width={300}
+        height={300}
+        alt="Mark Sikaundi"
+        className="rounded-full object-cover h-48 w-48 absolute bottom-0 right-0 mb-20 mr-20"
+      />
+
+
+      {/* Text or other content */}
+      <div className="absolute inset-0 flex items-center justify-center px-20">
+        <p className="text-xl self-center text-center">
+          to build talent pipelines and solve the world’s pressing
+          challenges with machine learning & data science.
+        </p>
+      </div>
+
+
+
     </div>
-  )
+  );
 }
