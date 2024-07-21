0aa1791f9599f58555a459435bd82978dd5a41dc
marksikaundi <emmanuelmark117@gmail.com>
2024-07-21 15:54:58 +0200

    refactor: Update MenuBar component to include H2, H3, and H4 buttons

--- components/TextEdit/Editor.jsx
+++ components/TextEdit/Editor.jsx
@@ -1,35 +1,62 @@
-'use client'
+"use client";
 
-import StarterKit  from "@tiptap/starter-kit";
-import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from "@tiptap/starter-kit";
+import { useEditor, EditorContent } from "@tiptap/react";
 import React from "react";
 import { Button } from "@/components/ui/button";
 
-
-
-export const MenuBar = ({editor}) => {
-  if(!editor) return null;
+export const MenuBar = ({ editor }) => {
+  if (!editor) return null;
 
   return (
     <div className="flex flex-wrap gap-5">
-      <Button onClick={() => editor.chain().focus().toggleHeading({level: 1}).run()}
-      variant={
-        editor.isActive('heading', {level: 1}) ? 'primary' : 'secondary'}
-      type="button">H1</Button>
-
+      <Button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        variant={
+          editor.isActive("heading", { level: 1 }) ? "primary" : "secondary"
+        }
+        type="button"
+      >
+        H1
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        variant={
+          editor.isActive("heading", { level: 2 }) ? "primary" : "secondary"
+        }
+        type="button"
+      >
+        H2
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        variant={
+          editor.isActive("heading", { level: 3 }) ? "primary" : "secondary"
+        }
+        type="button"
+      >
+        H3
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        variant={
+          editor.isActive("heading", { level: 4 }) ? "primary" : "secondary"
+        }
+        type="button"
+      >
+        H4
+      </Button>
     </div>
-  )
-
-}
-
+  );
+};
 
 export function TipTapEditor() {
   const editor = useEditor({
-    extensions: [StarterKit]
+    extensions: [StarterKit],
   });
   return (
     <div>
       <MenuBar editor={editor} />
     </div>
-  )
-} 
+  );
+}
