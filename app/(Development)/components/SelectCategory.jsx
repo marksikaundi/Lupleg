9d1b7406ecbdd6a844625dca87f916820782cfb2
marksikaundi <emmanuelmark117@gmail.com>
2024-07-21 15:00:50 +0200

    refactor: Update SelectCategory component to use grid layout

--- app/(Development)/components/SelectCategory.jsx
+++ app/(Development)/components/SelectCategory.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Card, CardHeader } from "@/components/ui/card";
+const { Globe, ChefHat, PartyPopper } = require("lucide-react");
+
+const categoryItems = [
+  { id: 0, name: "templates", title: "Templates", image: <Globe /> },
+  {
+    id: 1,
+    name: "uikit",
+    title: "UI Kit",
+    image: <ChefHat />,
+  },
+  {
+    id: 2,
+
+    name: "Icon",
+    title: "Icon",
+    image: <PartyPopper />,
+  },
+];
 
 export function SelectCategory() {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'> 
-        
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      {categoryItems.map((item) => (
+        <Card>
+          <CardHeader>
+            {item.image}
+            <h3 className="text-lg font-semibold">{item.title}</h3>
+          </CardHeader>
+        </Card>
+      ))}
     </div>
-  )
+  );
 }
