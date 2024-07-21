f5b4990ea69530963bce30bb329aa75e2083dab2
marksikaundi <emmanuelmark117@gmail.com>
2024-07-21 13:29:40 +0200

    refactor: Update SelectCategory component to use grid layout

--- app/(Development)/components/SelectCategory.jsx
+++ app/(Development)/components/SelectCategory.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export function SelectCategory() {
   return (
-    <div>
-      <h2>Hello from category</h2>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'> 
+        
     </div>
   )
 }
