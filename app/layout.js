9b40201f6b6dad07dc1b9974f151731155ff33bd
marksikaundi <emmanuelmark117@gmail.com>
2024-07-23 00:57:10 +0200

    refactor: Remove NavBar component and update layout to use Header component

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar/>
-        {/* <Header /> */}
+        <Header />
         {children}
         <Footer />
       </body>
--- app/page.js
+++ app/page.js
@@ -1,5 +1,38 @@
-import React from "react";
+import ProgramSummary from "@/components/FeaturesCards/ProgramSummary";
+import ShortMentorships from "@/components/HeroCards/ShortMentorships";
+import Link from "next/link";
 
-export default function page() {
-  return <div>Hello from Home</div>;
+export default function Home() {
+  return (
+    <div className="bg-[#FFFFFF]">
+      <div className="max-w-6xl mx-auto pt-25 pb-25 px-4 sm:px-6 lg:px-8 py-12 text-center">
+        <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-gray-800">
+          <span className="block xl:inline">The efficiency of help and</span>{" "}
+          <span className="block text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-orange-400 xl:inline">
+            Collaboration with
+          </span>{" "}
+          <span className="block xl:inline">Developers</span>{" "}
+          <span className="block text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 xl:inline">
+            empowers
+          </span>
+        </h1>
+
+        {/* TODO: For future uncomment this code */}
+        {/* <p className="mt-4 p-12 text-base sm:text-lg md:text-2xl text-gray-600">
+          Lupleg is a community platform that enables teams to streamline and
+          help novice scale up their career in tech.
+        </p> */}
+        <button className="W-15 mt-8 px-8 py-3 text-md bg-green-950 font-semibold border-2 border-green-950 p-6 text-white rounded  hover:bg-green-950 focus:outline-none focus:ring-2 focus:ring-orange focus:ring-opacity-50 transition duration-300">
+          <Link href="challenge">Register </Link>
+        </button>
+        <br />
+      </div>
+      <ShortMentorships />
+      <div className="my-8">
+        <hr className="border-t-2 border-gray-300" />
+      </div>
+
+      <ProgramSummary />
+    </div>
+  );
 }
