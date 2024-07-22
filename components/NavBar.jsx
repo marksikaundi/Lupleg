fd5d3db1a6e2d7fdbd338827e672710f7c15b152
marksikaundi <emmanuelmark117@gmail.com>
2024-07-22 21:22:44 +0200

    WORKING On nav

---
+++ components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav className='relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7'>
+        <div className='md:col-3'>
+            <Link href="/">
+            <h1>MarshalUI</h1>
+            </Link>
+
+        </div>
+      
+    </nav >
+  )
+}
