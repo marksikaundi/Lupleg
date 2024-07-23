3a299d0a4225e9c9567c9b1bd10b0170a0ef5d32
marksikaundi <emmanuelmark117@gmail.com>
2024-07-23 03:10:55 +0200

    refactor: Update UserNav component to include user image in dropdown menu

--- components/UserNav.jsx
+++ components/UserNav.jsx
@@ -7,39 +7,42 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
-export default function UserNav() {
+export default function UserNav({ email, name, userImage }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-            <Avatar className="h-10 w-10">
-            <AvatarFallback>MAR</AvatarFallback>
-
-            </Avatar>
+          <Avatar className="h-10 w-10">
+            <AvatarImage src={userImage} alt="User Image" />
+            <AvatarFallback> {name.slice(0, 3)} </AvatarFallback>
+          </Avatar>
         </Button>
       </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-            <DropdownMenuLabel className="font-normal">
-                <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">Mark Sikaundi</p>
-                    <p className="text-xs leading-none text-muted-foreground">mark@lupleg.website</p>
-
-                </div>
-
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenu>
-                <DropdownMenuItem href="/account">Account</DropdownMenuItem>
-                <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
-                <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
-                <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
-            </DropdownMenu>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem> <LogoutLink>Sign out</LogoutLink> </DropdownMenuItem>
-        </DropdownMenuContent>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{name}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenu>
+          <DropdownMenuItem href="/account">Account</DropdownMenuItem>
+          <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
+          <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
+          <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
+        </DropdownMenu>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          {" "}
+          <LogoutLink>Log out</LogoutLink>{" "}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
