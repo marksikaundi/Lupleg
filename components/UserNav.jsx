5c0dbdfce1270485aa33c30d6ba95ece617ece60
marksikaundi <emmanuelmark117@gmail.com>
2024-07-23 02:42:30 +0200

    refactor: Update UserNav component to include LogoutLink in dropdown menu

--- components/UserNav.jsx
+++ components/UserNav.jsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function UserNav() {
   return (
@@ -23,12 +24,21 @@ export default function UserNav() {
         <DropdownMenuContent className="w-56" align="end" forceMount>
             <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
-                    <p>Mark Sikaundi</p>
-                    <p>mark@lupleg.website</p>
+                    <p className="text-sm font-medium leading-none">Mark Sikaundi</p>
+                    <p className="text-xs leading-none text-muted-foreground">mark@lupleg.website</p>
 
                 </div>
 
             </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenu>
+                <DropdownMenuItem href="/account">Account</DropdownMenuItem>
+                <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
+                <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
+                <DropdownMenuItem href="/settings">Settings</DropdownMenuItem>
+            </DropdownMenu>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem> <LogoutLink>Sign out</LogoutLink> </DropdownMenuItem>
         </DropdownMenuContent>
     </DropdownMenu>
   );
