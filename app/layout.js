857da33fd0f550f29b93d92da0b63fa5e909f789
marksikaundi <emmanuelmark117@gmail.com>
2024-07-24 00:22:59 +0200

    refactor: Add NextSSRPlugin for lazy loading images and update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import { Inter } from "next/font/google";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -15,6 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <Header />
         {children}
         <Footer />
