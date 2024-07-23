9cde0a273933bb1d459b847efc606d59d3a06021
marksikaundi <emmanuelmark117@gmail.com>
2024-07-23 02:25:31 +0200

    refactor: Update @radix-ui/react-avatar to version 1.1.0

--- components/UserNav.jsx
+++ components/UserNav.jsx
@@ -1,16 +1,25 @@
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-  
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
 export default function UserNav() {
   return (
-    <div className=''>
-      
-    </div>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+            <Avatar className="h-10 w-10">
+            <AvatarFallback>MAR</AvatarFallback>
+
+            </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 }
