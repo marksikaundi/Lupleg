1ac8860af61cdf5e23cf1dcda2384ba703ef977a
marksikaundi <emmanuelmark117@gmail.com>
2024-07-23 03:18:24 +0200

    refactor: Update Header component layout and remove unused code

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,7 @@
-// Header.js
-
 import { HeaderClient } from "@/lib/HeaderClient";
 import { HeaderServer } from "@/lib/HeaderServer";
-
 
 export default async function Header() {
   const user = await HeaderServer();
   return <HeaderClient user={user} />;
 }
-
