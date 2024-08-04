587a9c9d4b532d27e23d91f0cffebe373ca2223f
marksikaundi <emmanuelmark117@gmail.com>
2024-08-04 13:52:45 +0200

    UPDATES: working on new landing page.

---
+++ components/sections/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section>
+      <div className="container">
+        <div className="">
+          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight ">
+            Version 2.0 is here
+          </div>
+          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6  ">
+            Pathway to productivity
+          </h1>
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6 ">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and help you reach your goals.
+          </p>
+          <div className="flex gap-1 items-center mt-[30px] ">
+            <button className="">Get for free</button>
+            <button className="">Learn more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
