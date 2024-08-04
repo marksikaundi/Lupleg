6213cb5740ab71b69ae5eff9beac9e942d3073e8
marksikaundi <emmanuelmark117@gmail.com>
2024-08-04 14:59:06 +0200

    fix: Update Hero background gradient
    The Hero component's background gradient was updated to use a 100% opacity for the second color stop. This ensures a consistent and visually appealing gradient effect on the landing page.

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -5,7 +5,7 @@ import CogImage from "@/assets/cog.png";
 
 export default function Hero() {
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)] ">
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] ">
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[#478px]">
@@ -27,10 +27,17 @@ export default function Hero() {
           </div>
           <div className="mt-20 md:mt-0 md:h-[#648px] md:flex-1 relative ">
             <Image
-              //   src="/cog.png"
               src={CogImage}
               alt="cog"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 "
+            />
+
+            <Image
+              src={CylinderImage}
+              alt="cylinder"
+              className="hidden md:block -top-8 -left-32 md:absolute  "
+              width={220}
+              height={220}
             />
           </div>
         </div>
