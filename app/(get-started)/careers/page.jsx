c693fc91f11b694e6c014b9272e329659d80cad1
marksikaundi <emmanuelmark117@gmail.com>
2024-08-14 12:39:34 +0200

    chore: Update button styles in Footer component

--- app/(get-started)/careers/page.jsx
+++ app/(get-started)/careers/page.jsx
@@ -221,7 +221,7 @@ export default function Careers() {
                       <div className="flex justify-end">
                         <Link
                           href="mailto:recruit@lupleg.website"
-                          className="inline-flex h-9 items-center justify-center rounded-md bg-[#C24229] px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#C24229] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                          className="inline-flex h-9 items-center justify-center rounded-md bg-[#F3A833] px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#F3A833] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                           prefetch={false}
                         >
                           Apply
@@ -252,7 +252,7 @@ export default function Careers() {
                       <div className="flex justify-end">
                         <Link
                           href="mailto:recruit@lupleg.website"
-                          className="inline-flex h-9 items-center justify-center rounded-md bg-[#C24229] px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#C24229] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                          className="inline-flex h-9 items-center justify-center rounded-md bg-[#F3A833] px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#F3A833] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                           prefetch={false}
                         >
                           Apply
@@ -283,7 +283,7 @@ export default function Careers() {
                       <div className="flex justify-end">
                         <Link
                           href="mailto:recruit@lupleg.website"
-                          className="inline-flex h-9 items-center justify-center rounded-md bg-[#C24229] px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#C24229] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                          className="inline-flex h-9 items-center justify-center rounded-md bg-[#F3A833] px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#F3A833] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                           prefetch={false}
                         >
                           Apply
