f6ed19b4521bd4327bc321d19c95716e91d2eb02
marksikaundi <emmanuelmark117@gmail.com>
2024-09-08 17:15:19 +0200

    chore: Update TypeScript tutorials page with improved styling and component changes

--- app/changelogs/data.js
+++ app/changelogs/data.js
@@ -1,33 +1,42 @@
 const changelogData = [
-    {
-      id: 1,
-      title: "Optimization UI For Mobile",
-      content: "Verification process update",
-      date: "2024-09-08",
-      avatars: 0
-    },
-    {
-      id: 2,
-      title: "Verification",
-      content: "Verification process update",
-      date: "2024-09-08",
-      avatars: 0
-    },
-    {
-      id: 3,
-      title: "Sign up or sign in with a One-Time Password (OTP)",
-      content: "Sign up or sign in with email using one-time passwords. This update offers three key benefits: Enhanced security, Frictionless sign-ups/sign-ins, No more 'I forgot my password'.",
-      date: "2024-09-07",
-      avatars: 2
-    },
-    {
-      id: 4,
-      title: "REST API for the Vercel Firewall",
-      content: "The Vercel Firewall now has a REST API, allowing you to: Create and manage Firewall configurations, Add and remove IP addresses, Enable or disable WAF (Coming Soon), Create and manage a list of blocked IP addresses.",
-      date: "2024-09-06",
-      avatars: 1
-    }
-  ];
-  
-  export default changelogData;
-  
+  {
+    id: 1,
+    title: "Working on improving the frontend design",
+    content:
+    "We are working on improving the frontend design of the application. This update will include a new color scheme, new fonts, and a new layout.",
+    date: "2024-09-09",
+    avatars: 0,
+  },
+  {
+    id: 2,
+    title: "Optimization UI For Mobile",
+    content: "Verification process update",
+    date: "2024-09-08",
+    avatars: 0,
+  },
+  {
+    id: 3,
+    title: "Verification",
+    content: "Verification process update",
+    date: "2024-09-08",
+    avatars: 0,
+  },
+  {
+    id: 4,
+    title: "Sign up or sign in with a One-Time Password (OTP)",
+    content:
+      "Sign up or sign in with email using one-time passwords. This update offers three key benefits: Enhanced security, Frictionless sign-ups/sign-ins, No more 'I forgot my password'.",
+    date: "2024-09-07",
+    avatars: 2,
+  },
+  {
+    id: 5,
+    title: "REST API for the Vercel Firewall",
+    content:
+      "The Vercel Firewall now has a REST API, allowing you to: Create and manage Firewall configurations, Add and remove IP addresses, Enable or disable WAF (Coming Soon), Create and manage a list of blocked IP addresses.",
+    date: "2024-09-06",
+    avatars: 1,
+  },
+];
+
+export default changelogData;
