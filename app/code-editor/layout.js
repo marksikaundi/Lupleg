21f2784741c5e3cefa3b50e821e0026335110961
marksikaundi <emmanuelmark117@gmail.com>
2024-10-09 12:01:34 +0200

    Refactor layout.js: Remove unused imports and clean up code

---
+++ app/code-editor/layout.js
@@ -0,0 +1,29 @@
+// import { Inter } from "next/font/google";
+// import { Poppins } from "next/font/google";
+
+// import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   weight: ["300", "400", "700"],
+// });
+
+export const metadata = {
+  title: "Building Africa's Tech Future",
+  description:
+    "Join us in shaping the future of technology in Africa! Our developer community empowers Africans with the skills and resources to solve problems, drive innovation, and create a more sustainable future.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body >
+        {children}
+      </body>
+    </html>
+  );
+}
