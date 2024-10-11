24bea0d3b5a4fbe7a48cfe4ea64a9d6b4af4f92f
marksikaundi <emmanuelmark117@gmail.com>
2024-10-11 10:15:23 +0200

    CORE: fixed ALT

--- app/programs/ps-card.jsx
+++ app/programs/ps-card.jsx
@@ -11,7 +11,7 @@ export default function PsCard() {
             <div className="flex flex-col sm:flex-row items-center">
               <img
                 src="/ps/dev.svg"
-                alt="TypeScript logo with glowing effect"
+                alt="Software Engineering at Lupleg"
                 className="mb-4 sm:mb-0 sm:mr-6 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"
                 width={120}
                 height={120}
