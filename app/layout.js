9d73ca8e30a8665ab54d81d2f0cd1040b19fec98
marksikaundi <emmanuelmark117@gmail.com>
2024-11-26 15:44:33 +0200

    Replace Header with Nav component and update Home section to use HomeHero

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header />
+        {/* <Header /> */}
+        <Nav />
         {children}
         <Footer />
       </body>
--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@ import WhatWeDo from "@/components/NewHero/WhatWeDo";
 import DemoView from "@/components/NewHero/DemoView";
 import New from "@/components/NewHero/New";
 import Programming from "@/components/HeroCards/Programming";
+import HomeHero from "@/components/HomeHero";
 
 export default function Home() {
   return (
     <div className="bg-[#FFFFFF]">
-      <New />
+      <HomeHero />
+      {/* <New /> */}
       <Programming />
       {/* <WelcomeHero /> */}
       {/* <WhatWeDo /> */}
