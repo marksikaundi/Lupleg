5748eb6426981e33eaf4df8170db6f97aa566f66
marksikaundi <emmanuelmark117@gmail.com>
2024-12-05 17:23:21 +0200

    Add new changelog entries and update UI design elements for improved aesthetics

--- app/changelogs/data.js
+++ app/changelogs/data.js
@@ -1,22 +1,28 @@
-import { data } from "autoprefixer";
 
 const changelogData = [
   {
     id: 0,
-    title: "Fixing and Improving the the entire UI of our landing page",
+    title: "Transforming the entire UI to new design",
     content:
-      "The entire UI of our landing page is being fixed and improved. We are working on making the landing page more user-friendly and visually appealing. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
-    data: "2024-09-12",
+      " We are transforming the entire UI of our application to a new design. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
+    data: "2024-12-05",
   },
   {
     id: 1,
+    title: "Fixing and Improving the the entire UI of our landing page",
+    content:
+      "The entire UI of our landing page is being fixed and improved. We are working on making the landing page more user-friendly and visually appealing. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
+    data: "2024-10-12",
+  },
+  {
+    id: 2,
     title: "Working on programs page to fix all broken links",
     content:
       "After our previous update, we noticed that some links on the programs page were broken. We are working on fixing all the broken links and making sure that you have access to all the information you need.",
     date: "2024-09-10",
   },
   {
-    id: 2,
+    id: 3,
     title: "Working on improving the frontend design",
     content:
       "We are working on improving the frontend design of the application. This update will include a new color scheme, new fonts, and a new layout.",
@@ -24,7 +30,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 3,
+    id: 4,
     title: "Changed the programs structure",
     content:
       "We have changed the structure of the programs to make it easier to navigate and find the information you need.",
@@ -32,7 +38,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 4,
+    id: 5,
     title: "Redesigned the header section",
     content:
       "We have redesigned the header section of the application to make it more user-friendly and visually appealing.",
@@ -40,7 +46,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 5,
+    id: 6,
     title: "Our Landing page improved",
     content:
       " All the information you need is now available on the landing page. You can easily find the programs, courses, and opportunities that interest you.",
@@ -48,7 +54,7 @@ const changelogData = [
     avatars: 2,
   },
   {
-    id: 6,
+    id: 7,
     title: "Custom structure for our programs and courses",
     content:
       " Our company has introduced a custom structure for our programs and courses. This will make it easier for you to find the information you need and navigate the application",
