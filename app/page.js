4c1c3b487afec82c6912971a3cb39c1e05beeb91
marksikaundi <emmanuelmark117@gmail.com>
2024-12-09 08:20:19 +0200

    CORE: made changes to components

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Programming />
       {/* <WelcomeHero /> */}
       {/* <WhatWeDo /> */}
-      <DemoView />
+      {/* <DemoView /> */}
       <ShortMentorships />
     </div>
   );
