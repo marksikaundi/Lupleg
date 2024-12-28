a69e36d7b8293396f0aadf760013f0fcc24a42b8
marksikaundi <emmanuelmark117@gmail.com>
2024-12-28 09:06:48 +0200

    CORE: remove unused imports in Building page for cleaner code

--- app/docs/building/page.jsx
+++ app/docs/building/page.jsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { ChevronDown, Home } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { IoHome } from "react-icons/io5";
 
 export default function Building() {
