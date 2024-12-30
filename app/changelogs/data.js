edfe5ceec9fc9d845516b768e2ba975c5a2d946e
marksikaundi <emmanuelmark117@gmail.com>
2024-12-30 02:10:16 +0200

    CORE: update changelog data with new entries and improved content

--- app/changelogs/data.js
+++ app/changelogs/data.js
@@ -2,27 +2,34 @@
 const changelogData = [
   {
     id: 0,
+    title: "Fixing UI issues and broken links",
+    content:
+      "We are fixing all the UI issues, old contents, and broken links. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that theys full oparational.",
+    data: "2024-12-30",
+  },
+  {
+    id: 1,
     title: "Transforming the entire UI to new design",
     content:
       " We are transforming the entire UI of our application to a new design. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
     data: "2024-12-05",
   },
   {
-    id: 1,
+    id: 2,
     title: "Fixing and Improving the the entire UI of our landing page",
     content:
       "The entire UI of our landing page is being fixed and improved. We are working on making the landing page more user-friendly and visually appealing. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
     data: "2024-10-12",
   },
   {
-    id: 2,
+    id: 3,
     title: "Working on programs page to fix all broken links",
     content:
       "After our previous update, we noticed that some links on the programs page were broken. We are working on fixing all the broken links and making sure that you have access to all the information you need.",
     date: "2024-09-10",
   },
   {
-    id: 3,
+    id: 4,
     title: "Working on improving the frontend design",
     content:
       "We are working on improving the frontend design of the application. This update will include a new color scheme, new fonts, and a new layout.",
@@ -30,7 +37,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 4,
+    id: 5,
     title: "Changed the programs structure",
     content:
       "We have changed the structure of the programs to make it easier to navigate and find the information you need.",
@@ -38,28 +45,12 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 5,
+    id: 6,
     title: "Redesigned the header section",
     content:
       "We have redesigned the header section of the application to make it more user-friendly and visually appealing.",
     date: "2024-08-10",
     avatars: 0,
-  },
-  {
-    id: 6,
-    title: "Our Landing page improved",
-    content:
-      " All the information you need is now available on the landing page. You can easily find the programs, courses, and opportunities that interest you.",
-    date: "2024-07-25",
-    avatars: 2,
-  },
-  {
-    id: 7,
-    title: "Custom structure for our programs and courses",
-    content:
-      " Our company has introduced a custom structure for our programs and courses. This will make it easier for you to find the information you need and navigate the application",
-    date: "2024-07-11",
-    avatars: 1,
   },
 ];
 
