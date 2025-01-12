ce6e6666ed9bac07579d5055d6fc5ec29a2fb416
marksikaundi <emmanuelmark117@gmail.com>
2025-01-12 16:47:55 +0200

    CORE: rename Docs component to CodeDaysChallenges for clarity

--- app/30dayschallenge/page.jsx
+++ app/30dayschallenge/page.jsx
@@ -7,7 +7,7 @@ import { FaFire } from "react-icons/fa";
 
 import Image from "next/image";
 
-export default function Docs() {
+export default function CodeDaysChallenges() {
   return (
     <div className="min-h-screen flex flex-col">
       {/* Top Navigation */}
