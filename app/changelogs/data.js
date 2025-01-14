9719e0f03ed4214486925f4171b4c66b0053d482
marksikaundi <emmanuelmark117@gmail.com>
2025-01-14 15:06:20 +0200

    CORE: update changelog data for 30DaysChallenge content development and link fixes

--- app/changelogs/data.js
+++ app/changelogs/data.js
@@ -1,35 +1,24 @@
 
 const changelogData = [
+  
   {
     id: 0,
-    title: "Fixing UI issues and broken links",
+    title: "30DaysChallenge - content development",
     content:
-      "We are fixing all the UI issues, old contents, and broken links. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that theys full oparational.",
-    data: "2024-12-30",
+    "Actively working on contents from challenges, to solutions in all the 30DaysChallenge we have on the platform.",
+    date: "2025-01-14",
+    avatars: 0,
   },
   {
     id: 1,
-    title: "Transforming the entire UI to new design",
+    title: "Working on programs page to fix all broken links",
     content:
-      " We are transforming the entire UI of our application to a new design. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
-    data: "2024-12-05",
+      "After our previous update, we noticed that some links on the programs page were broken.",
+    date: "2024-09-10",
+    avatars: 0,
   },
   {
     id: 2,
-    title: "Fixing and Improving the the entire UI of our landing page",
-    content:
-      "The entire UI of our landing page is being fixed and improved. We are working on making the landing page more user-friendly and visually appealing. This update will include a new color scheme, new fonts, and a new layout. We are also fixing all the broken links and making sure that you have access to all the information you need.",
-    data: "2024-10-12",
-  },
-  {
-    id: 3,
-    title: "Working on programs page to fix all broken links",
-    content:
-      "After our previous update, we noticed that some links on the programs page were broken. We are working on fixing all the broken links and making sure that you have access to all the information you need.",
-    date: "2024-09-10",
-  },
-  {
-    id: 4,
     title: "Working on improving the frontend design",
     content:
       "We are working on improving the frontend design of the application. This update will include a new color scheme, new fonts, and a new layout.",
@@ -37,7 +26,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 5,
+    id: 3,
     title: "Changed the programs structure",
     content:
       "We have changed the structure of the programs to make it easier to navigate and find the information you need.",
@@ -45,7 +34,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 6,
+    id: 4,
     title: "Redesigned the header section",
     content:
       "We have redesigned the header section of the application to make it more user-friendly and visually appealing.",
