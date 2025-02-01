d3f0901aa95ba958000935fadbf61d4e16918bbf
marksikaundi <emmanuelmark117@gmail.com>
2025-02-01 00:33:07 +0000

    🗑️ Remove unused Header component import from layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@
 import { Poppins } from "next/font/google";
 
 import "./globals.css";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 
