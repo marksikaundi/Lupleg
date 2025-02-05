a022175c4b4a017669fb7af867b3fee9142adc42
marksikaundi <emmanuelmark117@gmail.com>
2025-02-05 12:18:57 +0000

    📝 Update heading text from "Office 365" to "Microsoft Office 365"

--- app/programs/digital-skills/ad-card.jsx
+++ app/programs/digital-skills/ad-card.jsx
@@ -19,7 +19,7 @@ export default function AsCard() {
               <div>
                 <div className="flex flex-col sm:flex-row items-center sm:items-start mb-2">
                   <h2 className="text-xl sm:text-2xl text-white font-bold mb-2 sm:mb-0 sm:mr-3 text-center sm:text-left">
-                    Office 365
+                    Microsoft Office 365
                   </h2>
                   <span className="bg-[#F3A833] text-black text-xs font-bold px-2 py-1 rounded">
                     NEW
