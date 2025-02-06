1fa325ce46b65fd7314fb4b9b37458e81ff21c6d
marksikaundi <emmanuelmark117@gmail.com>
2025-02-06 08:44:52 +0200

    🎉 Update changelog with new program content and restructure entries for clarity

--- app/changelogs/data.js
+++ app/changelogs/data.js
@@ -1,16 +1,22 @@
-
 const changelogData = [
-  
   {
     id: 0,
-    title: "30DaysChallenge - content development",
+    title: "Programs Content Shift ",
     content:
-    "Actively working on contents from challenges, to solutions in all the 30DaysChallenge we have on the platform.",
-    date: "2025-01-14",
+      " We have shifted the content of the programs to make it more user-friendly and visually appealing.",
+    date: "2025-02-06",
     avatars: 0,
   },
   {
     id: 1,
+    title: "30DaysChallenge - content development",
+    content:
+      "Actively working on contents from challenges, to solutions in all the 30DaysChallenge we have on the platform.",
+    date: "2025-01-14",
+    avatars: 0,
+  },
+  {
+    id: 2,
     title: "Working on programs page to fix all broken links",
     content:
       "After our previous update, we noticed that some links on the programs page were broken.",
@@ -18,27 +24,11 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 2,
+    id: 3,
     title: "Working on improving the frontend design",
     content:
       "We are working on improving the frontend design of the application. This update will include a new color scheme, new fonts, and a new layout.",
     date: "2024-09-02",
-    avatars: 0,
-  },
-  {
-    id: 3,
-    title: "Changed the programs structure",
-    content:
-      "We have changed the structure of the programs to make it easier to navigate and find the information you need.",
-    date: "2024-08-16",
-    avatars: 0,
-  },
-  {
-    id: 4,
-    title: "Redesigned the header section",
-    content:
-      "We have redesigned the header section of the application to make it more user-friendly and visually appealing.",
-    date: "2024-08-10",
     avatars: 0,
   },
 ];
