63f4919736cb5e2be0fd7c430b00f13b44810278
marksikaundi <emmanuelmark117@gmail.com>
2025-02-08 11:26:17 +0000

    Feb 8, 2025, 1:26 PM

--- components/HeroCards/ShortMentorships.jsx
+++ components/HeroCards/ShortMentorships.jsx
@@ -7,26 +7,26 @@ export default function ShortCourseCard() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
           <div>
             <h1 className="text-3xl font-extrabold tracking-tight text-[#2D1537] ">
-              Easy short Courses
+             Short courses available
             </h1>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <p className="mt-4 max-w-2xl text-xl text-black lg:mx-auto">
               We have a variety of short courses that are designed to help you
               to learn and grow in your career. We have courses in Data Science,
               Machine Learning, Web Development, Mobile Development, and many
               more.
             </p>
           </div>
-          <div className="bg-gradient-to-br from-[#fde68a] to-[#fca5a5] p-8 rounded-xl border-[#2D1537] border-b-8 ">
-            <h2 className="text-3xl font-extrabold text-[#2D1537] ">
+          <div className="bg-[#2D1537] p-8 rounded-xl border-[#2D1537] border-b-8 ">
+            <h2 className="text-3xl font-extrabold text-white ">
               At your own pace or with a mentor
             </h2>
-            <p className="mt-4 text-lg text-gray-900">
+            <p className="mt-4 mb-2 text-lg text-white">
               Our courses are designed to be taken at your own pace, but you can
               also get a mentor to help you along the way. Our mentors are
               experts in their fields and are here to help you.
             </p>
             <Link
-              className="mt-6 text-base font-semibold text-black hover:text-black"
+              className="mt-6 text-base font-semibold text-white hover:text-white"
               href="https://mentor.lupleg.org"
             >
               Learn more →
