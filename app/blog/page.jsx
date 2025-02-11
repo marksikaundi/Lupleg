91742d9012cd9096afc9d50aa3af5e093ef829dd
marksikaundi <emmanuelmark117@gmail.com>
2025-02-11 22:53:40 +0000

    🔄 Update blog post image URL and date

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -32,12 +32,12 @@ const posts = [
     excerpt:
       "Deep dive into advanced JavaScript concepts through practical kata examples and expert solutions.",
     image:
-      "https://www.lupleg.org/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F7c679591-6991-4402-bc63-0ee97010fc6f-1g.png&w=640&q=75",
+      "https://10fc6f-1g.png&w=640&q=75",
     author: {
       name: "Mark Sikaundi",
       avatar: "/placeholder.svg",
     },
-    date: "February 01, 2025",
+    date: "February 12, 2025",
     slug: "mastering-javascript-kata",
     category: "Programming",
   },
