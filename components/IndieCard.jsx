b00c723aa664078480c5b2900b6e7c9831717110
marksikaundi <emmanuelmark117@gmail.com>
2025-02-14 14:57:26 +0200

    🎉 Replace IndieBoosting component with IndieCard for better modularity

---
+++ components/IndieCard.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { IndieBoosting } from "@indieboosting/react";
+
+const IndieCard = () => {
+  return <IndieBoosting channelId="Q2ODFY2OLI" />;
+};
+
+export default IndieCard;
