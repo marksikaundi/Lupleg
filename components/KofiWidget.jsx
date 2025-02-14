cc54521166d21ca1abd8753cd9f2836cc092b882
marksikaundi <emmanuelmark117@gmail.com>
2025-02-14 21:50:51 +0000

    ✨ Add KofiWidget component

---
+++ components/KofiWidget.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import Script from 'next/script';
+
+const KofiWidget = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.kofiWidgetOverlay) {
+      window.kofiWidgetOverlay.draw('lupleg', {
+        'type': 'floating-chat',
+        'floating-chat.donateButton.text': 'Support Us',
+        'floating-chat.donateButton.background-color': '#00b9fe',
+        'floating-chat.donateButton.text-color': '#fff'
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <Script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js" strategy="lazyOnload" />
+    </>
+  );
+};
+
+export default KofiWidget;
