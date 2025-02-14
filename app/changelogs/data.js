4e9ce033fe07d5cf972d450799fe634ed53daff7
marksikaundi <emmanuelmark117@gmail.com>
2025-02-14 16:41:03 +0200

    🎉 Update changelog data with new entries and reassign IDs for improved organization

--- app/changelogs/data.js
+++ app/changelogs/data.js
@@ -1,6 +1,15 @@
 const changelogData = [
   {
-    id: 0,
+    id: 1,
+    title: "Frontend free challenges UI/UX update",
+    content:
+      "Layout and content shift (UI/UX) of the frontend free challenges to make it more user-friendly.",
+    date: "2025-02-14",
+    avatars: 0,
+  },
+
+  {
+    id: 2,
     title: "Programs Content Shift ",
     content:
       " We have shifted the content of the programs to make it more user-friendly and visually appealing.",
@@ -8,7 +17,7 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 1,
+    id: 3,
     title: "30DaysChallenge - content development",
     content:
       "Actively working on contents from challenges, to solutions in all the 30DaysChallenge we have on the platform.",
@@ -16,19 +25,11 @@ const changelogData = [
     avatars: 0,
   },
   {
-    id: 2,
+    id: 4,
     title: "Working on programs page to fix all broken links",
     content:
       "After our previous update, we noticed that some links on the programs page were broken.",
     date: "2024-09-10",
-    avatars: 0,
-  },
-  {
-    id: 3,
-    title: "Working on improving the frontend design",
-    content:
-      "We are working on improving the frontend design of the application. This update will include a new color scheme, new fonts, and a new layout.",
-    date: "2024-09-02",
     avatars: 0,
   },
 ];
